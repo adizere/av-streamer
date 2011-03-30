46cd6b229aa1ea0be1d3ea03809ce0009963fe51
paulmegabyte <paulmegabyte@4deb675f-0649-05f5-30af-d0d0c803fc22>
2011-03-30 19:20:18 +0000

    AV-Streamer server project

---
+++ AvsServer/main.cpp
@@ -0,0 +1,17 @@
+/* 
+ * File:   main.cpp
+ * Author: megabyte
+ *
+ * Created on March 30, 2011, 10:01 PM
+ */
+
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
---
+++ common/common.h
@@ -0,0 +1,15 @@
+/* 
+ * File:   common.h
+ * Author: megabyte
+ *
+ * Created on March 30, 2011, 10:08 PM
+ */
+
+#ifndef _COMMON_H
+#define	_COMMON_H
+
+// TODO include fifo.h, rtp.h ...
+
+
+#endif	/* _COMMON_H */
+
