a494e49d5f93183ccbf54f91e341bc9435351e36
paulmegabyte <paulmegabyte@4deb675f-0649-05f5-30af-d0d0c803fc22>
2011-04-16 19:49:43 +0000

    mai multe headere + wrapper pentru perror

--- common/common.h
+++ common/common.h
@@ -12,13 +12,29 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+#include <pthread.h>
+#include <signal.h>
+#include <time.h>
+
 //General defines and includes needed
+#include <sys/socket.h>
+#include <sys/types.h>
 #include <arpa/inet.h>
+#include <netinet/in.h>
+
 #include <errno.h>
+
 #define SOCK_DCCP 6
 #define IPPROTO_DCCP 33  //it must be this number. This number is assigned by IANA to DCCP
 #define SOL_DCCP 269
 #define MAX_DCCP_CONNECTION_BACK_LOG 5
+
+void error(const char* msg)
+{
+    perror(msg);
+    fflush(stderr);
+    fflush(stdout);
+}
 
 
 #include "fifo.h"
