e1fea9788bfb99f80603eed4f0741893e9111e54
paulmegabyte <paulmegabyte@4deb675f-0649-05f5-30af-d0d0c803fc22>
2011-04-16 19:23:20 +0000

    Headere generale de care avem nevoie

--- common/common.h
+++ common/common.h
@@ -8,8 +8,18 @@
 #ifndef _COMMON_H
 #define	_COMMON_H
 
+#include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
+
+//General defines and includes needed
+#include <arpa/inet.h>
+#include <errno.h>
+#define SOCK_DCCP 6
+#define IPPROTO_DCCP 33  //it must be this number. This number is assigned by IANA to DCCP
+#define SOL_DCCP 269
+#define MAX_DCCP_CONNECTION_BACK_LOG 5
+
 
 #include "fifo.h"
 #include "rtp.h"
