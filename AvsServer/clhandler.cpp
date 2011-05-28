609f064c37dad7f7fa323d7abccec20adca59b3f
turnerjohannes@gmail.com <turnerjohannes@gmail.com@4deb675f-0649-05f5-30af-d0d0c803fc22>
2011-05-28 02:12:38 +0000

    fifo_elem is now a real AVMediaPacket with added thread support

--- AvsServer/clhandler.cpp
+++ AvsServer/clhandler.cpp
@@ -1,6 +1,18 @@
 #include "clhandler.h"
 
 
+// Securely free client handler data.
+void ch_data_free(ch_data *data)
+{
+    if (data == NULL)
+        return;
+
+    if (data->avmanager != NULL)
+        free (data->avmanager);
+    free (data);
+}
+
+
 /* Client handler thread
  */
 void* ch_thread(void* data)
@@ -38,7 +50,7 @@ void* ch_thread(void* data)
 
     close(client_sock);
     destroy_fifo(dp->private_fifo);
-    free(dp);
+    ch_data_free(dp);
 
 
     LOCK(&server_mutex);
@@ -60,6 +72,7 @@ void* send_thread(void* data)
 
     fifo_elem fe;
     rtp_packet* packet = create_packet();
+    AVMediaPacket *media_packet = NULL; // AVMediaPacket alias/pointer to fifo_elem.
 
     sequence_nr = SEQUENCE_START;
     timestamp_nr = TIMESTAMP_START;
@@ -78,14 +91,16 @@ void* send_thread(void* data)
             usleep(5 * dp->st_rate);
             continue;
         }
+
+        media_packet = fe;
         
         /* Create new rtp packet to be sent.. */
-        if (sizeof(fe) > PAYLOAD_MAX_SIZE) {
+        if (media_packet->entire_media_packet_size > PAYLOAD_MAX_SIZE) {
             int done = 0;
             
-            while(done < sizeof(fe)) {
+            while(done < media_packet->entire_media_packet_size) {
                 memset(packet->payload, 0, PAYLOAD_MAX_SIZE);
-                memcpy(packet->payload, (char*)&fe + done, PAYLOAD_MAX_SIZE - 1);
+                memcpy(packet->payload, (char*)fe + done, PAYLOAD_MAX_SIZE - 1);
                 
                 packet->header.seq = sequence_nr++;
                 packet->header.timestamp = timestamp_nr;
@@ -93,7 +108,7 @@ void* send_thread(void* data)
                 
                 done = done + PAYLOAD_MAX_SIZE - 1;
                 
-                if (done >= sizeof(fe))
+                if (done >= media_packet->entire_media_packet_size)
                     packet->header.M = MARKER_LAST;
                 
                 int rc = send(client_sock, packet, sizeof(*packet), 0);
@@ -105,11 +120,16 @@ void* send_thread(void* data)
             packet->header.timestamp = timestamp_nr;
             packet->header.M = MARKER_ALONE;
             
-            memcpy(packet->payload, (char*)&fe, sizeof(fe));
+            memcpy(packet->payload, (char*)fe, media_packet->entire_media_packet_size);
             
             int rc = send(client_sock, packet, sizeof(*packet), 0);
             if (rc < 0)
                 error("Error sending a packet to the client");
+            
+            // Free the AVMediaPacket dequeued from the queue.
+            AVManager::free_packet (media_packet);
+            media_packet = NULL;
+            fe = NULL;
         }
         timestamp_nr++;
 
@@ -146,16 +166,67 @@ void* recv_thread(void* data)
 /* Thread used to read the data streams and put them in the queue */
 void* stream_read_thread(void* data)
 {
-    ch_data* dp = (ch_data*)data;
-    pthread_mutex_t* p_fifo_mutex = dp->p_fifo_mutex;
+    ch_data* dp = (ch_data *)data;
+    pthread_mutex_t *p_fifo_mutex = dp->p_fifo_mutex;
+    AVMediaPacket *media_packet = NULL;
+    int queue_status = 0;
+    int max_enqueue_tries = MAX_ENQUEUE_TRIES;
+
+    // Init server-size codecs and open media file.
+    if (dp->avmanager->init_send (dp->filename) == false)
+        goto CLEAN_AND_EXIT;
+
+    // Get useful media information for client and push them in the queue.
+    if (dp->avmanager->get_stream_info (&dp->stream_info) == false)
+        goto CLEAN_AND_EXIT;
+    if (AVManager::stream_info_to_packet (&dp->stream_info, &media_packet))
+        goto CLEAN_AND_EXIT;
+    queue_status = enqueue (dp->private_fifo, (fifo_elem)media_packet);
+    if (queue_status < 0) {
+        DBGPRINT ("[stream_read_thread] Queue is full!");
+        goto CLEAN_AND_EXIT;
+    }
+
+    // Loop for media packets.
+    while (dp->avmanager->read_packet_from_file (&media_packet))
+    {
+        
+        
+        max_enqueue_tries = MAX_ENQUEUE_TRIES;
+        do {
+            LOCK (p_fifo_mutex);
+            queue_status = enqueue (dp->private_fifo, (fifo_elem)media_packet);
+            UNLOCK (p_fifo_mutex);
+            if (queue_status < 0)
+                sleep (1);
+            max_enqueue_tries--;
+        } while (queue_status < 0 && max_enqueue_tries > 0);
+
+        if (queue_status < 0) {
+            DBGPRINT ("[stream_read_thread] Queue is full!");
+            UNLOCK (p_fifo_mutex);
+            break;
+        }
+        
+        // Don't free the media packet each time, because the enqueue()
+        // method copies the reference, NOT the content of the fifo_elem.
+        
+        media_packet = NULL;
+    }
+
+CLEAN_AND_EXIT:
+    if (dp->avmanager != NULL) {
+        if (media_packet != NULL)
+            AVManager::free_packet (media_packet);
+            media_packet = NULL;
+        dp->avmanager->end_send ();
+    }
+
+/*
+    Dummy :p code. That's why we don't use it.
 
     fifo_elem fe;
-    rtp_packet* packet = (rtp_packet*)&fe;
-    /* Adi:
-     * Johannes should open dp->filename and populate the queue held in
-     * dp->private_fifo;
-     * Example: enqueue(dp->private_fifo, pkg), where pkg is of type rtp_packet
-     */
+    rtp_packet *packet = (rtp_packet *)&fe;
 
     // dummy code begin
     FILE* fh = fopen(dp->filename, "r");
@@ -174,4 +245,5 @@ void* stream_read_thread(void* data)
     }
     
     // dummy code end
+*/
 }
